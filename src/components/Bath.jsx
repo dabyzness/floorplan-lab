a812d5d80793ce4eceb2d9c0fb4da7fb15082219
Damian <bzdyrad@gmail.com>
2022-10-10 20:35:03 -0400

    alter bath component

--- src/components/Bath.jsx
+++ src/components/Bath.jsx
@@ -1,7 +1,7 @@
 const Bath = ({ size }) => {
   return (
     <div className={`bath-${size}`}>
-      <h1>{size[0].toUpperCase()} Bath</h1>
+      <h1>{size[0].toUpperCase() + size.slice(1)} Bath</h1>
     </div>
   );
 };
